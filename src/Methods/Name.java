bf55457a82ee26ddf9cb3b2f008152013bfe37e0
Marnala Gopi Vinay <vinayux8055@gmail.com>
2026-07-17 22:12:21 +0530

    Create a method that prints your name.

---
+++ src/Methods/Name.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Name {
+    static void Greet(String name)
+    {
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Greet("Vinay");
+    }
+}
