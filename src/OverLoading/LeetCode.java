73ef925d0475ca528c8d99f1102f06ed3787e797
Marnala Gopi Vinay <vinayux8055@gmail.com>
2026-07-18 23:41:14 +0530

    Leetcode probelm is Valid Anagram

---
+++ src/OverLoading/LeetCode.java
@@ -0,0 +1,34 @@
+package OverLoading;
+
+public class LeetCode {
+}
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        boolean[] used = new boolean[t.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            boolean found = false;
+
+            for (int j = 0; j < t.length(); j++) {
+
+                if (!used[j] && s.charAt(i) == t.charAt(j)) {
+                    used[j] = true;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
