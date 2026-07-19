9fd0bdccb70ef383f2e1174c973f14f3ed6e470d
Marnala Gopi Vinay <vinayux8055@gmail.com>
2026-07-19 15:17:35 +0530

    Recursion Factorial probelm in java

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -1,14 +1,14 @@
 package Recursion;
 
 public class Factorial {
-    static int Factorial(int Number)
+    static int FFactorial(int Number)
     {
         if(Number == 0 || Number == 1)
             return 1 ;
-        return  Number * Factorial(Number-1);
+        return  Number * FFactorial(Number-1);
     }
     public static void main(String[] args) {
 
-        System.out.println( Factorial(5));
+        System.out.println( FFactorial(5));
     }
 }
