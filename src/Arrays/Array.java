645ec4b32d4b1b2be1ae7005f5faf413bcaafcc1
Marnala Gopi Vinay <vinayux8055@gmail.com>
2026-07-20 22:21:45 +0530

    java Array's

---
+++ src/Arrays/Array.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Array {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,4,6,8};
+
+        for(int value : arr){
+
+            System.out.println(value);
+
+        }
+
+    }
+
+}
